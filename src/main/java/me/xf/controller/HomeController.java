d6f61104d99ee4cdf2b93e2a7266411dfb89de5a
fightX3 <953983040@qq.com>
2016-08-08 15:12:07 +0800

    更新  welcome访问

--- src/main/java/me/xf/controller/HomeController.java
+++ src/main/java/me/xf/controller/HomeController.java
@@ -14,6 +14,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value="/")
 public class HomeController {
+	/**
+	 * web默认访问页面
+	 * @Description: 
+	 * @author xf
+	 * @date 2016年8月8日下午3:11:06 
+	 * @return
+	 */
+	@RequestMapping(value="")
+	public String welcome(){
+		return "forward:/index";
+	}
 	
 	@RequestMapping(value="index")
 	public String index(){
